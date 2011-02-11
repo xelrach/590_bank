4a7ccfc46d88e32138c104e1ab837b45aff28a62
Pete Stein <pete@petevm.(none)>
2011-02-11 16:37:24 -0500

    Initial commit of branch server.

--- Branch_Server.java
+++ Branch_Server.java
@@ -1,48 +1,234 @@
-import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class Branch_Server {
-	public static void main(String[] args) {
-		String name="", port="", gui_ip="", gui_port="";
-		ArrayList<Branch> branches = new ArrayList<Branch>();
 
-		for (int i = 0; i < args.length; i++) {
-			if (args[i].equals("--name") || args[i].equals("-n")) {
-				name = args[i+1];
-				i++;
-				continue;
-			}
-			if (args[i].equals("--port") || args[i].equals("-p")) {
-				port = args[i+1];
-				i++;
-				continue;
-			}
-			if (args[i].equals("--gui") || args[i].equals("-g")) {
-				String[] branch = args[i+1].split(":");
-				gui_ip = branch[0];
-				gui_port = branch[1];
-				i++;
-				continue;
-			}
-			if (args[i].equals("--branch") || args[i].equals("--remote") || args[i].equals("-r")) {
-				System.out.println(args[i+1]);
-				String[] branch = args[i+1].split(":");
-				Branch b = new Branch(branch[0], branch[1], Integer.parseInt(branch[2].trim()) );
-				branches.add(b);
-				i++;
-				continue;
-			}
-		}
-/*		System.out.println("Server");
-		System.out.println(name);
-		System.out.println(port);
-		System.out.println(gui_ip);
-		System.out.println(gui_port);*/
-		String str_id = "Server: " + name + ":" + port + " " + gui_ip + ":" + gui_port;
-		System.out.println(str_id);
+	public int port = 4444;
+	public ServerThread serverThread;
 
-		System.out.println("Other Branches (" + branches.size() + ")");
-		for (int i = 0; i<branches.size(); i++) {
-			System.out.println(branches.get(i));
+	public HashMap<String, Account> accounts = new HashMap<String, Account>();
+
+	public String process_input(String input) {
+		String answer = "";
+
+		String[] tokens = input.split(" ");
+		String messageID = "";
+
+		char messageType = 0;
+		String command = "";
+		String accountID = "";
+		String arg4 = "";
+		String arg5 = "";
+
+		if (tokens.length > 0) {
+			messageID = tokens[0];
+
+			for (int i = 0; i < tokens.length; i++) {
+				tokens[i] = tokens[i].trim();
+			}
+
+			messageType = messageID.charAt(0);
+			messageID = messageID.substring(1); // strip message type character
 		}
+
+		if (tokens.length > 1)
+			command = tokens[1];
+
+		if (tokens.length > 2)
+			accountID = tokens[2];
+
+		if (tokens.length > 3)
+			arg4 = tokens[3];
+
+		if (tokens.length > 4)
+			arg5 = tokens[4];
+
+		if (messageType == 'c') {
+			// client request
+
+			System.out.println("Command is [" + command + "]");
+
+			if (command.equals("w"))
+				answer = withdrawal(accountID, Float.parseFloat(arg4));
+			else if (command.equals("d"))
+				answer = deposit(accountID, Float.parseFloat(arg4));
+			else if (command.equals("t"))
+				answer = transfer(accountID, arg4, Float.parseFloat(arg5));
+			else if (command.equals("q"))
+				answer = query(accountID);
+		}
+
+		answer = "s" + messageID + " " + answer;
+		return answer;
+    	}
+
+	public String withdrawal(String accountID, float amount) {
+		String answer = "error";
+
+		if (!validAccount(accountID)) {
+			answer = "error-account";
+			return answer;
+		}
+
+		if (amount < 0) {
+			answer = "error-invalid";
+			return answer;
+		}
+
+		Account account = accounts.get(accountID);
+		account.addBalance(-amount);
+	
+		answer = "ok";
+
+		return answer;
 	}
+
+
+	public String deposit(String accountID, float amount) {
+		String answer = "error";
+
+		System.out.println("deposit");
+
+		if (!validAccount(accountID)) {
+			answer = "error-account";
+			return answer;
+		}
+
+		if (amount < 0) {
+			answer = "error-invalid";
+			return answer;
+		}
+
+		Account account = accounts.get(accountID);
+		account.addBalance(amount);
+	
+		answer = "ok";
+
+		return answer;
+	}
+
+	public boolean validAccount(String accountID) {
+		System.out.println("Checking validity of [" + accountID + "]");
+
+		Account account = accounts.get(accountID);
+
+		if (account == null)
+			System.out.println("tried to get account [" + accountID + "] but it's null.");
+
+		return account != null;
+    	}
+
+	public String transfer(String accountFromID, String accountToID, float amount) {
+		String answer = "error";
+
+		if (!validAccount(accountFromID)) {
+			answer = "error-from";
+			return answer;
+		}
+
+		if (!validAccount(accountToID)) {
+			answer = "error-to";
+			return answer;
+		}
+
+		Account accountFrom = accounts.get(accountFromID);
+		Account accountTo = accounts.get(accountToID);
+
+		if (amount < 0) {
+			answer = "error-invalid";
+			return answer;
+		}
+
+		if (accountFrom.getBalance() < amount) {
+			answer = "error-insufficient";
+			return answer;
+		}
+
+		accountFrom.addBalance(-amount);
+		accountTo.addBalance(amount);
+
+		answer = "ok";
+
+		return answer;
+	}
+
+	public String query(String accountID) {
+		String answer = "error";
+
+		Account account = null;
+
+		if (!validAccount(accountID)) {
+			account = new Account(accountID);
+			System.out.println("Creating account with id [" + accountID + "] putting in " + account);
+			accounts.put(accountID, account);
+		}
+
+		answer = "q" + " " + accountID + " " + account.getBalance();
+
+		return answer;
+ 	}
+
+}
+
+
+class ServerThread implements Runnable {
+    protected DatagramSocket socket = null;
+    protected BufferedReader in = null;
+    protected boolean moreQuotes = true;
+
+    public int port;
+
+
+    public ServerThread(int port) throws IOException {
+	this("ServerThread", port);
+    }
+
+    public ServerThread(String name, int port) throws IOException {
+	this.port = port;
+        socket = new DatagramSocket(port);
+
+        try {
+            in = new BufferedReader(new FileReader("one-liners.txt"));
+        } catch (FileNotFoundException e) {
+            System.err.println("Could not open quote file. Serving time instead.");
+        }
+    }
+
+
+    public void run() {
+
+        while (moreQuotes) {
+            try {
+                byte[] buf = new byte[256];
+                byte[] inbuf = new byte[256];
+
+                    // receive request
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+
+		
+		// figure out response
+		String input = new String(buf);
+                String dString = process_input( input );
+
+		
+		
+
+                buf = dString.getBytes();
+
+		    // send the response to the client at "address" and "port"
+                InetAddress address = packet.getAddress();
+                int port = packet.getPort();
+                packet = new DatagramPacket(buf, buf.length, address, port);
+                socket.send(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+		moreQuotes = false;
+            }
+        }
+	System.out.println("Closing socket.");
+        socket.close();
+    }
+
 }
